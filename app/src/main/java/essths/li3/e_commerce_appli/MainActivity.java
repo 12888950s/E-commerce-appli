5dbcf329952e66abce55d927c0ac8757a721d7bd
taasniim <labiditasnim50@gmail.com>
2023-11-25 18:39:47 +0100

    toast

--- app/src/main/java/essths/li3/e_commerce_appli/MainActivity.java
+++ app/src/main/java/essths/li3/e_commerce_appli/MainActivity.java
@@ -3,6 +3,7 @@ package essths.li3.e_commerce_appli;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "this our commercial app", Toast.LENGTH_SHORT).show();
     }
 }
